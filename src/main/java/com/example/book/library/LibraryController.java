d8c8860a6145f6686d01daad53016db0decf8930
yongwa1214 <yongwa1214@gmail.com>
2026-07-22 18:57:52 +0900

    library post test2

--- src/main/java/com/example/book/library/LibraryController.java
+++ src/main/java/com/example/book/library/LibraryController.java
@@ -9,11 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("bobo")
 public class LibraryController {
     private final LibraryService libraryService;
 
--- src/main/java/com/example/book/library/model/LibraryReq.java
+++ src/main/java/com/example/book/library/model/LibraryReq.java
@@ -1,12 +1,16 @@
 package com.example.book.library.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Builder
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class LibraryReq {
     private Integer memberId;
     private String status;
